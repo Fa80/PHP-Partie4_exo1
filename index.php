a1ff8796a84fb6c3211638477ad82d8bc8bc8e72
Fa <ngomfa@gmail.com>
2018-11-26 15:18:14 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<!-- Faire une fonction qui retourne true. -->
+<body>
+  <?php
+  function bool (){ // nommage de ma fonction bool (binaire: true ou false)
+  return true; // retourner ma valeur true
+  }
+  echo bool(); // afficher ma fonction
+  ?>
+</body>
+</html>
